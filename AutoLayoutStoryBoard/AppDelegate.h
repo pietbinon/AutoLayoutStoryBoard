07fce046168230a62443b6e56e84c2a6641935a8
pietbinon <pierrebinon@icloud.com>
2017-01-17 14:28:08 -0500

    Initial Commit

---
+++ AutoLayoutStoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStoryBoard
+//
+//  Created by Pierre Binon on 2017-01-17.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
